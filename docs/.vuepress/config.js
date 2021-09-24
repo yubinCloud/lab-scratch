0b93f9f3892cb7d84657dfb18e22afaa7aabcdb3
yubinCloud <yubin_SkyWalker@yeah.net>
2021-09-24 13:12:39 +0800

    #dev 完成爬虫部分

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: 'vdoing', // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-  title: "Evan's blog",
-  description: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。',
+  title: "lab scratch",
+  description: '实验室入门教程',
   base: '/lab-scratch/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
