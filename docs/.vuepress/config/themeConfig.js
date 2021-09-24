3b6b33e06c8c4531ac41f3a323809abfd4dccbd2
yubinCloud <yubin_SkyWalker@yeah.net>
2021-09-24 18:40:07 +0800

    #dev 添加了 Git 的使用

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -52,7 +52,7 @@ module.exports = {
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
-    avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
+    avatar: '/img/avatar.jpg',
     name: 'INET',
     slogan: '实验室入门教程',
   },
